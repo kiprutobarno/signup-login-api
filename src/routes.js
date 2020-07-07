1ff636745ab949ea683ef41597ca54de7e63c6ec
ywalakamar <barxwells@gmail.com>
2020-07-07 15:29:53 +0300

    feature(user): display all users

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,13 @@
 import { Router } from "express";
-import { createUser, deleteUser } from "./controllers/userController";
+import {
+  createUser,
+  deleteUser,
+  displayUsers,
+} from "./controllers/userController";
 
 const routes = Router();
 routes.post("/users", createUser);
 routes.delete("/users/:email", deleteUser);
+routes.get("/users", displayUsers);
 
 export default routes;
