70c86949bd24283d4998493ef27c70b6003d8794
ywalakamar <barxwells@gmail.com>
2020-07-10 02:39:35 +0300

    chore(authorization): use cookies

--- src/routes.js
+++ src/routes.js
@@ -5,11 +5,12 @@ import {
   displayUsers,
   login
 } from './controllers/userController';
+import {authMiddleware} from "./middleware/auth"
 
 const routes = Router();
 routes.post('/users/register', createUser);
 routes.delete('/users/:email', deleteUser);
-routes.get('/users', displayUsers);
+routes.get('/users', authMiddleware, displayUsers);
 routes.post('/users/login', login);
 
 export default routes;
