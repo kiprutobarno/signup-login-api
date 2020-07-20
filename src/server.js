42a88d85150d1016917063e4edb572f75685097a
ywalakamar <barxwells@gmail.com>
2020-07-20 10:56:34 +0300

    chore(linting): run linter

--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
-import express from "express";
-import dotenv from "dotenv";
-import cookieParser from "cookie-parser";
-import cors from "cors";
-import router from "./routes";
+import express from 'express';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import router from './routes';
 
 dotenv.config();
 const app = express();
