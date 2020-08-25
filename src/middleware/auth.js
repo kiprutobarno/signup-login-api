9da432d279785c741e086354ce877a7de6d70542
ywalakamar <barxwells@gmail.com>
2020-08-25 23:34:52 +0300

    chore(linting):run linter

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,30 +1,20 @@
-import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
-import dotenv from "dotenv";
-import { search } from "../models/userModel";
-import db from "../utils/db";
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+import { search } from '../models/userModel';
+import db from '../utils/db';
 
 dotenv.config();
 
 const encrypt = (password) => bcrypt.hashSync(password, bcrypt.genSaltSync(10));
 
-const decrypt = (password, encryptedPassword) =>
-  bcrypt.compareSync(password, encryptedPassword);
+const decrypt = (password, encryptedPassword) => bcrypt.compareSync(password, encryptedPassword);
 
 const generateJwtToken = (user) => {
   const expiresIn = 60 * 10;
   const secret = process.env.SECRET;
   const data = { id: user.id, email: user.email, isAdmin: user.is_admin };
   return { expiresIn, token: jwt.sign(data, secret, { expiresIn }) };
-};
-
-const createCookie = (res, data) => {
-  const cookieData = {
-    expiresOn: new Date(Date.now() + data.expiresIn),
-    secure: false,
-    httpOnly: true,
-  };
-  return res.cookie("token", data.token, cookieData);
 };
 
 const authMiddleware = async (req, res, next) => {
@@ -45,4 +35,6 @@ const authMiddleware = async (req, res, next) => {
   }
 };
 
-export { encrypt, decrypt, generateJwtToken, createCookie, authMiddleware };
+export {
+  encrypt, decrypt, generateJwtToken, authMiddleware
+};
